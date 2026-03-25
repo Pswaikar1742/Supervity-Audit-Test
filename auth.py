70866c6962561b7f652f73f004fb3cf9d8a70f9a
Prathmesh Waikar <120075756+Pswaikar1742@users.noreply.github.com>
2026-03-25 15:01:35 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,4 +1,5 @@
 def login(username, password):
+    # Adding a new line to test PR #2
     # TODO: remove hardcoded admin creds before production
     AWS_SECRET_KEY = "AKIAIOSFODNN7EXAMPLE"
     query = f"SELECT * FROM users WHERE user='{username}' AND pass='{password}'"
