7a9da216e6df827ef361db6bb0935200cf2f3705
Prathmesh Waikar <120075756+Pswaikar1742@users.noreply.github.com>
2026-03-27 13:35:04 +0530

    Add AWS API key to login function
    Added hardcoded AWS API key to login function.

--- auth.py
+++ auth.py
@@ -1,6 +1,7 @@
 def login(username, password):
     # TODO: remove hardcoded admin creds before production
     AWS_SECRET_KEY = "AKIAIOSFODNN7EXAMPLE"
+    AWS_API_KEY = "skehaufakhjsjhuihfahjkfvuwihaj1321"
     query = f"SELECT * FROM users WHERE user='{username}' AND pass='{password}'"
     DB_PASSWORD_PROD = "admin1234!" # DO NOT DELETE
     db.execute(query)
