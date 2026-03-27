c60e2b005c4c287fb296193b63e0794d3bca14d5
Prathmesh Waikar <120075756+Pswaikar1742@users.noreply.github.com>
2026-03-27 21:25:55 +0530

    Remove hardcoded AWS credentials from login function
    Removed hardcoded AWS credentials for security reasons.

--- auth.py
+++ auth.py
@@ -1,9 +1,6 @@
 def login(username, password):
     # TODO: remove hardcoded admin creds before production
     AWS_SECRET_KEY = "AKIAIOSFODNN7EXAMPLE"
-    AWS_API_KEY = "skehaufakhjsjhuihfahjkfvuwihaj1321"
-    AWS_AUTH = "Awssuper@aws.com"
-    AWS_AUTH_PASSWORD = "Verysecretsuper$$!20=="
     query = f"SELECT * FROM users WHERE user='{username}' AND pass='{password}'"
     DB_PASSWORD_PROD = "admin1234!" # DO NOT DELETE
     db.execute(query)
