1912183c00a7f7508eb72e8f6e54c8daf3ae3efb
Prathmesh Waikar <120075756+Pswaikar1742@users.noreply.github.com>
2026-03-27 13:21:31 +0530

    Add hardcoded DB password for production testing
    Added hardcoded production database password for testing.

--- auth.py
+++ auth.py
@@ -2,5 +2,6 @@ def login(username, password):
     # TODO: remove hardcoded admin creds before production
     AWS_SECRET_KEY = "AKIAIOSFODNN7EXAMPLE"
     query = f"SELECT * FROM users WHERE user='{username}' AND pass='{password}'"
+    DB_PASSWORD_PROD = "admin1234!" # DO NOT DELETE
     db.execute(query)
     return True
