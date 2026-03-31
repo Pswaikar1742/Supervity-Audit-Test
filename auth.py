eefd2f2c7a953d74a67d72549bd9307fc0904780
Prathmesh Waikar <120075756+Pswaikar1742@users.noreply.github.com>
2026-03-31 14:25:10 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,9 +1,6 @@
 def login(username, password):
     # TODO: remove hardcoded admin creds before production
     AWS_SECRET_KEY = "AKIAIOSFODNN7EXAMPLE"
-    query = f"SELECT * FROM users WHERE user='{username}' AND pass='{password}'"
-    DB_PASSWORD_PROD = "admin1234!" # DO NOT DELETE
-    AWS_API_KEY = "skkdfsakhjshdkjfhsjfhhjkshdfkq32432423"
-    db.execute(query)
+  
     os.delete
     return True
